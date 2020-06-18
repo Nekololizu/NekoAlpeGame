6082aa25ab6cd3d14f4f5d127a5e0925b55300c6
Nekololizu <nekololizu@gmail.com>
2020-06-18 21:54:44 +0200

    updated player.gd
    Added better movement

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,15 +1,21 @@
 extends KinematicBody2D
 
+const ACCELERATION = 100
+const MAX_SPEED = 100
+const FRICTION = 10
+
 var velocity = Vector2.ZERO
 
-func _physics_process(delta):  
+func _physics_process(delta): # If something changes & is connected to frame rate you have to multiplie"*" with delta
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized() #Makes moving in multiple directions the same speed as moving in 1 direction
 	
 	if input_vector != Vector2.ZERO:
-		velocity = input_vector
+		velocity += input_vector * ACCELERATION * delta
+		velocity = velocity.clamped(MAX_SPEED * delta)
 	else:
-		velocity = Vector2.ZERO
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	move_and_collide(velocity)
