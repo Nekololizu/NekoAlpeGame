1c58309b802a6baa321e2b1b81252d158def88ff
Nekololizu <nekololizu@gmail.com>
2020-06-19 02:40:19 +0200

    Player Attack
    Added Attack animation & state

--- Player/Player.gd
+++ Player/Player.gd
@@ -4,13 +4,34 @@ const ACCELERATION = 500 # how fast the player gains speed
 const MAX_SPEED = 80 # how fast the player can move
 const FRICTION = 500 # how long it takes for the player to lose speed when not moving
 
+enum {
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
+var state = MOVE
 var velocity = Vector2.ZERO 
 
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 
+func _ready():
+	animationTree.active = true
+
 func _physics_process(delta): # If something changes & is connected to frame rate you have to multiplie"*" with delta
+	match state:
+		MOVE:
+			move_state(delta)
+		
+		ROLL:
+			pass
+		
+		ATTACK:
+			attack_state(delta)
+
+func move_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
@@ -19,6 +40,7 @@ func _physics_process(delta): # If something changes & is connected to frame rat
 	if input_vector != Vector2.ZERO: # If the input vector is not equal to vector2
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationTree.set("parameters/Attack/blend_position", input_vector)
 		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	
@@ -27,3 +49,12 @@ func _physics_process(delta): # If something changes & is connected to frame rat
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	velocity = move_and_slide(velocity)
+	
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK
+
+func attack_state(delta):
+	velocity = Vector2.ZERO
+	animationState.travel("Attack")
+func attack_animation_finished():
+	state = MOVE
