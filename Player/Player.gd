571f1e46f72297a11f0def1e3695793f4dbcae7e
Nekololizu <nekololizu@gmail.com>
2020-06-28 21:41:18 +0200

    Added Enemy & knockback
    Bat enemy & knockback function

--- Player/Player.gd
+++ Player/Player.gd
@@ -18,9 +18,11 @@ var roll_vector = Vector2.LEFT
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
+onready var SwordHitBox = $HitboxPivot/SwordHitbox
 
 func _ready():
 	animationTree.active = true
+	SwordHitBox.knockback_vector = roll_vector
 
 func _physics_process(delta): # If something changes & is connected to frame rate you have to multiplie"*" with delta
 	match state:
@@ -41,6 +43,7 @@ func move_state(delta):
 	
 	if input_vector != Vector2.ZERO: # If the input vector is not equal to vector2
 		roll_vector = input_vector
+		SwordHitBox.knockback_vector = input_vector
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
 		animationTree.set("parameters/Attack/blend_position", input_vector)
