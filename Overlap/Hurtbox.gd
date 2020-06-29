55f41be9239981ae583eea06183f57b6b6db10de
Nekololizu <nekololizu@gmail.com>
2020-06-29 21:52:31 +0200

    p17
    player stats & enemy attack

--- Overlap/Hurtbox.gd
+++ Overlap/Hurtbox.gd
@@ -1,12 +1,36 @@
 extends Area2D
 
-export(bool) var show_hit = true
-
 const HitEffect = preload("res://Effects/HitEffect.tscn")
 
-func _on_Hurtbox_area_entered(area):
-	if show_hit:
-		var effect = HitEffect.instance()
-		var main = get_tree().current_scene
-		main.add_child(effect)
-		effect.global_position = global_position
+var iframe = false setget set_iframe
+
+onready var timer = $Timer
+
+signal iframe_started
+signal iframe_ended
+
+func set_iframe(value):
+	iframe = value
+	if iframe == true:
+		emit_signal("iframe_started")
+	else:
+		emit_signal("iframe_ended")
+
+func start_iframe(duration):
+	self.iframe = true
+	timer.start(duration)
+
+func create_hit_effect():
+	var effect = HitEffect.instance()
+	var main = get_tree().current_scene
+	main.add_child(effect)
+	effect.global_position = global_position
+
+func _on_Timer_timeout():
+	self.iframe = false
+
+func _on_Hurtbox_iframe_started():
+	set_deferred("monitorable", false)
+
+func _on_Hurtbox_iframe_ended():
+	set_deferred("monitorable", true)
