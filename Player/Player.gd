ae7e270e26be58f79c5913cddd0953ace25387cf
Nekololizu <nekololizu@gmail.com>
2020-06-30 12:19:12 +0200

    p21
    Bat Wander State

--- Player/Player.gd
+++ Player/Player.gd
@@ -23,6 +23,7 @@ onready var SwordHitBox = $HitboxPivot/SwordHitbox
 onready var hurtbox = $Hurtbox
 
 func _ready():
+	randomize()
 	stats.connect("no_health", self, "queue_free")
 	animationTree.active = true
 	SwordHitBox.knockback_vector = roll_vector
